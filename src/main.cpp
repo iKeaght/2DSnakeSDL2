0c49e33d21ad47b8a42bd5625ed8c7ac5c37f119
iKeaght <noelmatheo@yahoo.fr>
2023-11-21 14:15:59 +0100

    Handle inputs and boundaries

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,67 @@
 #include <SDL.h>
 #include <iostream>
-const int snakePartSize = 50;
+const int SnakePartSize = 50;
 const int ScreenWidth = 1920;
 const int ScreenHeight = 1080;
+struct Boundaries {
+	int xStart = SnakePartSize;
+	int yStart = SnakePartSize;
+	int xEnd = ScreenWidth - SnakePartSize;
+	int yEnd = ScreenHeight - SnakePartSize;
+};
+const int xBoundaries = ScreenWidth - SnakePartSize;
+const int yBoundaries = ScreenHeight - SnakePartSize;
+
+
+void HandleInputs(bool& running, Boundaries& bound, SDL_Event& event, SDL_Rect& rect) {
+	while (SDL_PollEvent(&event)) {
+		if (event.type == SDL_QUIT) {
+			running = false;
+		}
+		else if (event.type == SDL_KEYDOWN) {
+			switch (event.key.keysym.sym) {
+			case SDLK_RIGHT:
+				if (rect.x <= bound.xEnd) {
+					rect.x += 50;
+				}
+				break;
+			case SDLK_LEFT:
+				if (bound.xStart <= rect.x) {
+					rect.x -= 50;
+				}
+				break;
+
+			case SDLK_UP:
+				if (bound.yStart <= rect.y) {
+					rect.y -= 50;
+				}
+				break;
+
+			case SDLK_DOWN:
+				if (rect.y <= bound.yEnd) {
+					rect.y += 50;
+				}
+				break;
+			}
+
+		}
+	}
+}
+
 int main(int argc, char* args[]) {
+	Boundaries boundaries;
 	SDL_Window* window = nullptr;
 	SDL_Renderer* renderer = nullptr;
 	SDL_Event e;
 
 	SDL_Init(SDL_INIT_EVERYTHING);
 	bool running = true;
-	SDL_Rect r{ 10,10,snakePartSize,snakePartSize };
-	
-	SDL_CreateWindowAndRenderer(ScreenWidth, ScreenHeight, 0, &window, &renderer);
+	SDL_Rect r{ 1920 - 50,1080 - 50,SnakePartSize,SnakePartSize };
+
+	SDL_CreateWindowAndRenderer(ScreenWidth + 100, ScreenHeight + 100, 0, &window, &renderer);
 
 	while (running) {
-		while (SDL_PollEvent(&e)) {
-			if (e.type == SDL_QUIT) {
-				running = false;
-			}
-			else if (e.type == SDL_KEYDOWN) {
-				switch (e.key.keysym.sym) {
-				case SDLK_RIGHT:
-					r.x += 50;
-					break;
-				case SDLK_LEFT:
-					r.x -= 50;
-					break;
-
-				case SDLK_UP:
-					r.y -= 50;
-					break;
-
-				case SDLK_DOWN:
-					r.y += 50;
-					break;
-				}
-			}
-			else if (e.type == SDL_KEYUP) {
-				switch (e.key.keysym.sym) {
-				case SDLK_RIGHT:
-					std::cout << "Right key is released" << std::endl;
-					break;
-				}
-			}
-		}
+		HandleInputs(running, boundaries, e, r);
 		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 		SDL_RenderClear(renderer);
 
@@ -55,4 +72,39 @@ int main(int argc, char* args[]) {
 		//SDL_Delay(10);
 	}
 	return 0;
+}
+
+void Input(bool& running, Boundaries& bound, SDL_Event& event, SDL_Rect& rect) {
+	while (SDL_PollEvent(&event)) {
+		if (event.type == SDL_QUIT) {
+			running = false;
+		}
+		else if (event.type == SDL_KEYDOWN) {
+			switch (event.key.keysym.sym) {
+			case SDLK_RIGHT:
+				if (rect.x <= bound.xEnd) {
+					rect.x += 50;
+				}
+				break;
+			case SDLK_LEFT:
+				if (bound.xStart <= rect.x) {
+					rect.x -= 50;
+				}
+				break;
+
+			case SDLK_UP:
+				if (bound.yStart <= rect.y) {
+					rect.y -= 50;
+				}
+				break;
+
+			case SDLK_DOWN:
+				if (rect.y <= bound.yEnd) {
+					rect.y += 50;
+				}
+				break;
+			}
+
+		}
+	}
 }
