925bda9a2a69a7570df49ea455e9680b97023a0f
iKeaght <noelmatheo@yahoo.fr>
2023-11-21 16:45:32 +0100

    Snake head image and load

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <SDL.h>
+#include <SDL_image.h>
 #include <iostream>
-const int SnakePartSize = 50;
+
+const int SnakePartSize = 48;
 const int ScreenWidth = 1920;
 const int ScreenHeight = 1080;
+const char* IMAGE_PATH = "./Images/";
 enum Direction
 {
 	STOP,
@@ -29,6 +32,7 @@ void HandleInputs(SDL_Event& event, SDL_Rect& rect) {
 		if (event.type == SDL_QUIT) {
 
 			running = false;
+
 		}
 		else if (event.type == SDL_KEYDOWN) {
 			switch (event.key.keysym.sym) {
@@ -81,27 +85,52 @@ void MoveSnake(SDL_Rect& rect) {
 	}
 
 }
+
+SDL_Surface* LoadImage(SDL_Surface* image, const char* imageName) {
+	char fullPath[30];
+	strcpy_s(fullPath, IMAGE_PATH);
+	strcat_s(fullPath, imageName);
+	image = IMG_Load(fullPath);
+	if (!image) {
+		std::cout << "Image : " << imageName << "not loaded" << std::endl;
+	}
+	return image;
+
+}
 int main(int argc, char* args[]) {
 	SDL_Window* window = nullptr;
 	SDL_Renderer* renderer = nullptr;
+	SDL_Surface* snakeHead = nullptr;
+	SDL_Texture* snakeHeadTexture = nullptr;
 	SDL_Event e;
+	SDL_Rect startPositionRect{ 1920 - 50,1080 - 50,SnakePartSize,SnakePartSize };
 
 	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_Rect r{ 1920 - 50,1080 - 50,SnakePartSize,SnakePartSize };
 
 	SDL_CreateWindowAndRenderer(ScreenWidth + 100, ScreenHeight + 100, 0, &window, &renderer);
+
+	IMG_Init(IMG_INIT_PNG);
+	snakeHead = LoadImage(snakeHead, "SnakeHead.png");
+	snakeHeadTexture = SDL_CreateTextureFromSurface(renderer, snakeHead);
 
 	while (running) {
 		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 		SDL_RenderClear(renderer);
 
 		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-		SDL_RenderFillRect(renderer, &r);
+		SDL_RenderFillRect(renderer, &startPositionRect);
 
+		SDL_RenderCopy(renderer, snakeHeadTexture, NULL, &startPositionRect);
 		SDL_RenderPresent(renderer);
-		HandleInputs(e, r);
-		MoveSnake(r);
+		HandleInputs(e, startPositionRect);
+		MoveSnake(startPositionRect);
 		SDL_Delay(100);
 	}
+	SDL_FreeSurface(snakeHead);
+	SDL_DestroyTexture(snakeHeadTexture);
+	IMG_Quit();
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 	return 0;
 }
